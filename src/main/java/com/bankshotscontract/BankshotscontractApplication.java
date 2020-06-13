9b3199a47ae0b96eb3f409e41229fdb6b86d9a24
Dominique Jones <developerdomjones@gmail.com>
2020-06-13 09:18:30 -0400

    Initial Commit

---
+++ src/main/java/com/bankshotscontract/BankshotscontractApplication.java
@@ -0,0 +1,13 @@
+package com.bankshotscontract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankshotscontractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankshotscontractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankshotscontract/BankshotscontractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankshotscontract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankshotscontractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
